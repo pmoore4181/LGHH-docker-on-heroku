19d568082258d955519378fa76a179227a39eac8
Patrick <pmoore4181@gmail.com>
2019-10-22 19:56:34 -0500

    uncomment server.js

--- server.js
+++ server.js
@@ -3,27 +3,22 @@ const app = express();
 var path = require('path');
 
 app.use(express.static(path.join(__dirname, '/css'))); // contains css
-// app.use(express.static(__dirname + '/photos')); // contains images
+app.use(express.static(__dirname + '/photos')); // contains images
 app.use(express.static(__dirname + '/Javascript')); // contains js
-// app.use(express.static(__dirname + '/views')); //contains mainpage.html
-// app.use(express.static(__dirname + '/bower_components')); // contains bootstrap stuff
+app.use(express.static(__dirname + '/views')); //contains mainpage.html
+app.use(express.static(__dirname + '/bower_components')); // contains bootstrap stuff
 
 app.set('PORT', (process.env.PORT || 5000));
 
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-// app.get('/', function(request, response) {
-//     response.sendFile(path.join(__dirname, "views/lghh.html"));
-// });
-
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, "views/lghh.html"));
 });
 
 app.listen(app.get('PORT'));
 console.log('Running on port', app.get('PORT'));
-
